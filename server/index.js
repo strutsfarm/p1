81fc61f5b4bec3cc5e292622aa6c678fde1d3eed
Bjorn Agren <skyy@skyy.se>
2017-12-27 18:56:14 +0100

    t

--- server/index.js
+++ server/index.js
@@ -6,9 +6,9 @@ app.get('/temperature', function(req, res) {
 	res.send('24 C');
 });
 
-#app.get/'/humidity', function(req, res) {
-#	res.send('48%');
-#});
+app.get('/humidity', function(req, res) {
+	res.send('48%');
+});
 
 app.listen(3000, function() {
 	console.log('Server listening on port 3000');
